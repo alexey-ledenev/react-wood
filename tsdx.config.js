21434f7a15e178770d8a40a70ce2640293ad267c
Alexey L <20566780+alexey-ledenev@users.noreply.github.com>
2021-01-21 21:29:17 +0300

    add Postcss

---
+++ tsdx.config.js
@@ -0,0 +1,32 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  /**
+   * @param {import('rollup/dist/rollup').InputOptions} config
+   */
+  rollup(config, options) {
+    options.target = 'browser';
+    config.plugins.push(
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: [
+              'default',
+              {
+                discardComments: {
+                  removeAll: true,
+                },
+              },
+            ],
+          }),
+        ],
+        minimize: true,
+        use: ['sass'],
+      })
+    );
+    return config;
+  },
+};
