4ec084ceb27e90fbdde7070c59419fd1f82278fe
Alexey L <20566780+alexey-ledenev@users.noreply.github.com>
2021-01-22 22:24:54 +0300

    add Svgr to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,17 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
+  webpackFinal: async (config) => {
+    const assetRule = config.module.rules.find(({ test }) => test.test(".svg"));
+    const assetLoader = {
+      loader: assetRule.loader,
+      options: assetRule.options || assetRule.query
+    };
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", assetLoader]
+    });
+    return config;
+  },
 };
