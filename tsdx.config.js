511e23deab9e76cc537f34cd117ae403a183a31c
Alexey L <20566780+alexey-ledenev@users.noreply.github.com>
2021-01-22 23:38:01 +0300

    build fix

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,7 +1,7 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const svgr = require('@svgr/rollup');
+const svgr = require('@svgr/rollup').default;
 
 module.exports = {
   /**
